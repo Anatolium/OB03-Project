ffda7da2983e6797b5275e60e8b828f7665bc738
Анатолий <lan2002@yandex.ru>
2024-04-04 17:33:57 +0300

    update main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Композиция
+class Engine:
+    def start(self):
+        print("Двигатель запущен")
+
+    def stop(self):
+        print("Двигатель остановлен")
+
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+
+    def start(self):
+        self.engine.start()
+
+    def stop(self):
+        self.engine.stop()
+
+my_car = Car()
+my_car.start()
+my_car.stop()
+
+# Агрегация
+class Teacher:
+    def teach(self):
+        print("Преподаватель учит")
+
+class School:
+    def __init__(self, new_teacher):
+        self.teacher = new_teacher
+
+    def start_lesson(self):
+        self.teacher.teach()
+
+my_teacher = Teacher()
+my_school = School(my_teacher)
